6dea9c72b400a702c2d4d6335af4f8003c145528
arshiam2 <arshiam2@illinois.edu>
2018-10-03 11:10:28 -0500

    init

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
 
+<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
+=======
+ReactDOM.render(<App />, document.getElementById('root'));
+>>>>>>> init
