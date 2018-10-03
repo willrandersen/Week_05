384fce1952991d9cc99f2445aa0655fa9937cafd
arshiam2 <arshiam2@illinois.edu>
2018-10-03 12:20:41 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> init
